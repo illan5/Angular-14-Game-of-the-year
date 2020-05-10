08d4a24580c231d5111fc3771a0234316a28b2e2
Jose Illan <jose_illan@hotmail.com>
2020-05-10 20:17:00 +0200

    Dynamice bar-graph values randomly

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent {
+export class GraficoBarraHorizontalComponent implements OnDestroy{
 
   results: any[] = [
     {
@@ -39,11 +39,29 @@ export class GraficoBarraHorizontalComponent {
   yAxisLabel = 'Votos';
 
   colorScheme = 'nightLights';
+  intervalo;
 
-  constructor() { }
+  constructor() { 
+    
+    this.intervalo = setInterval( () => {
+      console.log('tick');
+
+      const newResults = [...this.results];
+
+      for( let i in newResults ){
+        newResults[i].value = Math.round( Math.random() * 500)
+      }
+      this.results = [...newResults];
+    }, 1500)
+
+  }
 
   onSelect(event) {
     console.log(event);
+  }
+
+  ngOnDestroy() {
+    clearInterval( this.intervalo );
   }
 
 }
