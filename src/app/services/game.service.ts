cf8899eaa82e7a8ba22ca1c75903b26087a926a9
Jose Illan <jose_illan@hotmail.com>
2020-05-12 23:49:42 +0200

    Improve service logic to reduce load time

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,15 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  private juegos: Game[] = [];
+
   constructor( private http: HttpClient ) { }
 
   getNominados() {
-    return this.http.get<Game[]>(`${ environment.url }/api/goty`)
+    if ( this.juegos.length > 0 ) {
+      // no tenemos juegos
+      console.log("Desde cache");
+      return of(this.juegos);
+    } else {
+      console.log("Desde servidor");
+      return this.http.get<Game[]>(`${ environment.url }/api/goty`)
+          .pipe(
+            tap(
+              juegos => this.juegos = juegos
+            )
+          );
+    }
+
   }
  }
